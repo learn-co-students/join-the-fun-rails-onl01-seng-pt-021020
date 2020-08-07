5a29d30c6d4e3c7167332d645bbc15817854bc2c
Awilliams4321 <awilliams1234@live.com>
2020-08-07 14:34:32 -0600

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,6 +2,10 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
+      t.integer  "taxi_id"
+      t.integer  "passenger_id"
+      t.datetime "created_at",   null: false
+      t.datetime "updated_at",   null: false
     end
   end
 end
